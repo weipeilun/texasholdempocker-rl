c45b94a56718788fa130d55f3bc94e7debf6c8b1
weipeilun <weipeilun0217@gmail.com>
2023-11-19 22:41:01 +0800

    修改更新模型失败的bug

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -2,7 +2,7 @@ def get_state_dict_from_model(model):
     state_dict = model.state_dict()
     new_state_dict = dict()
     for key, state_tensor in state_dict.items():
-        if not state_tensor.requires_grad:
-            continue
+        # if not state_tensor.requires_grad:
+        #     continue
         new_state_dict[key] = state_tensor.clone().cpu()
     return new_state_dict
