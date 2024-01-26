9d0bc32465f05ee920d5b82f24f9601869e9bf57
weipeilun <weipeilun0217@gmail.com>
2024-01-26 20:16:46 +0800

    try to fix tensorRT bugs

--- rl/models.py
+++ rl/models.py
@@ -97,10 +97,10 @@ class EnvEmbedding(nn.Module):
         batch_size = x.shape[0]
 
         item_idx_modified_array = x + self.field_start_idx_array
-        if self.num_starters > 0:
-            game_status_tensor = torch.hstack((self.starter_idx_array.unsqueeze(0).repeat(batch_size, 1), item_idx_modified_array))
-        else:
-            game_status_tensor = item_idx_modified_array
+        game_status_tensor = torch.hstack((self.starter_idx_array.unsqueeze(0).repeat(batch_size, 1), item_idx_modified_array))
+        # if self.num_starters > 0:
+        # else:
+        #     game_status_tensor = item_idx_modified_array
         # game_status_embedding = self.field_embedding(game_status_tensor)
         #
         # position_embedding = self.position_embedding(self.position_embedding_idx.repeat(batch_size, 1))
