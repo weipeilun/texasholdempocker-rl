20e2b93314316f9993ea2f6701a98865c671577d
weipeilun <weipeilun0217@gmail.com>
2024-01-26 18:14:53 +0800

    add support to tensorRT (in model and engine generating)

--- generate_default_model.py
+++ generate_default_model.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     # to regenerate new default model
     save_model(model, f'{model_name}.pth')
     # to regenerate new default onnx model
-    torch.onnx.export(model, torch.zeros(0, 28, dtype=torch.int32).to(model.device), f'{model_name}.onnx')
+    torch.onnx.export(model, torch.zeros(1, 28, dtype=torch.int32).to(model.device), f'{model_name}.onnx')
 
     TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
     trt_runtime = trt.Runtime(TRT_LOGGER)
