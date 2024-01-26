aa265792780f1ab102397b928175ad1071670059
weipeilun <weipeilun0217@gmail.com>
2024-01-26 18:32:26 +0800

    add support to tensorRT (in model and engine generating)

--- generate_default_model.py
+++ generate_default_model.py
@@ -11,10 +11,10 @@ EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
 def build_engine(model_file):
     builder = trt.Builder(TRT_LOGGER)
     network = builder.create_network(EXPLICIT_BATCH)
-
     config = builder.create_builder_config()
     config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, 1 << 20)
     parser = trt.OnnxParser(network, TRT_LOGGER)
+
     with open(model_file, 'rb') as model:
         assert parser.parse(model.read())
         return builder.build_serialized_network(network, config)
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     # to regenerate new default model
     save_model(model, f'{model_name}.pth')
     # to regenerate new default onnx model
-    torch.onnx.export(model, torch.zeros(1, 28, dtype=torch.int32).to(model.device), f'{model_name}.onnx')
+    torch.onnx.export(model, torch.zeros(1, 28, dtype=torch.int32).to(model.device), f'{model_name}.onnx', opset_version=13, enable_onnx_checker=False, do_constant_folding=True)
 
     TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
     trt_runtime = trt.Runtime(TRT_LOGGER)
