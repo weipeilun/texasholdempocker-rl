d2c0029ee3c43ac08505004951c348c4578bc987
weipeilun <weipeilun0217@gmail.com>
2024-01-26 17:44:46 +0800

    add support to tensorRT (in model and engine generating)

--- generate_default_model.py
+++ generate_default_model.py
@@ -1,5 +1,6 @@
 from env.workflow import *
 from tools.param_parser import *
+import tensorrt as trt
 
 
 if __name__ == '__main__':
@@ -9,10 +10,24 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     logger.setLevel(log_level)
 
+    model_name = 'models/default_train'
+
     args, params = parse_params()
     model_param_dict = params['model_param_dict']
     model = AlphaGoZero(**model_param_dict)
     # to regenerate new default model
-    save_model(model, 'models/default_train.pth')
+    save_model(model, f'{model_name}.pth')
+    # to regenerate new default onnx model
+    torch.onnx.export(model, torch.zeros((1, 28), dtype=torch.int32).to(model.device), f'{model_name}.onnx')
+
+    TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+    trt_runtime = trt.Runtime(TRT_LOGGER)
+
+    onnx_model = trt.OnnxParser(TRT_LOGGER)
+    with open(f"{model_name}.onnx", 'rb') as model_file:
+        onnx_model.parse(model_file.read())
+
+    engine = trt.Builder(TRT_LOGGER).build_cuda_engine(onnx_model.network)
+    trt_runtime.save_engine(engine, f"{model_name}.trt")
 
     logging.info('success')
