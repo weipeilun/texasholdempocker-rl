401e7c5938c29a192459bc304df58d5e14f2e49d
weipeilun <weipeilun0217@gmail.com>
2024-01-26 18:11:45 +0800

    add support to tensorRT (in model and engine generating)

--- generate_default_model.py
+++ generate_default_model.py
@@ -3,6 +3,19 @@ from tools.param_parser import *
 import tensorrt as trt
 
 
+EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+
+def build_engine(model_file):
+    builder = trt.Builder(TRT_LOGGER)
+    network = builder.create_network(EXPLICIT_BATCH)
+    config = builder.create_builder_config()
+    parser = trt.OnnxParser(network, TRT_LOGGER)
+
+    with open(model_file, 'rb') as model:
+        assert parser.parse(model.read())
+        return builder.build_serialized_network(network, config)
+
+
 if __name__ == '__main__':
     log_level = logging.INFO
     logging.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
@@ -18,26 +31,13 @@ if __name__ == '__main__':
     # to regenerate new default model
     save_model(model, f'{model_name}.pth')
     # to regenerate new default onnx model
-    torch.onnx.export(model, torch.zeros((-1, 28), dtype=torch.int32).to(model.device), f'{model_name}.onnx')
+    torch.onnx.export(model, (torch.zeros(28, dtype=torch.int32).to(model.device)), f'{model_name}.onnx')
 
     TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
     trt_runtime = trt.Runtime(TRT_LOGGER)
 
-    with trt.Builder(TRT_LOGGER) as builder, builder.create_network() as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
-        with open(f"{model_name}.onnx", 'rb') as model_file:
-            parser.parse(model_file.read())
-        profile = builder.create_optimization_profile()
-        # profile.set_shape(input_tensor_name, (1, 3, 224, 224), (max_batch_size, 3, 224, 224), (max_batch_size, 3, 224, 224))
-
-        config = builder.create_builder_config()
-        config.add_optimization_profile(profile)
-
-        trt_model_engine = builder.build_serialized_network(network, config)
-        # trt_model_context = trt_model_engine.create_execution_context()
-
-        # builder.build_cuda_engine(onnx_model.network)
-        # trt_runtime.save_engine(trt_model_engine, f"{model_name}.trt")
-        with open(f"{model_name}.trt", "wb") as f:
-            f.write(trt_model_engine.serialize())
+    trt_model_engine = build_engine(f"{model_name}.onnx")
+    with open(f"{model_name}.trt", "wb") as f:
+        f.write(trt_model_engine)
 
     logging.info('success')
