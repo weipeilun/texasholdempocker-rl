43c74ed33300adc8924e55c98ff02c15a28f141e
weipeilun <weipeilun0217@gmail.com>
2024-01-31 10:52:28 +0800

    fix bugs

--- utils/tensorrt_utils.py
+++ utils/tensorrt_utils.py
@@ -186,7 +186,7 @@ def reset_input_shape(context, input_name: str, input_shape: tuple):
 
 # Allocates all buffers required for an engine, i.e. host/device inputs/outputs.
 # If engine uses dynamic shapes, specify a profile to find the maximum input & output size.
-def allocate_buffers(engine: trt.ICudaEngine, profile_idx: Optional[int] = None):
+def allocate_buffers(engine: trt.ICudaEngine, profile_idx: Optional[int] = None, idx_in_profile: Optional[int] = None):
     inputs = []
     outputs = []
     bindings = []
@@ -195,7 +195,7 @@ def allocate_buffers(engine: trt.ICudaEngine, profile_idx: Optional[int] = None)
     for binding in tensor_names:
         # get_tensor_profile_shape returns (min_shape, optimal_shape, max_shape)
         # Pick out the max shape to allocate enough memory for the binding.
-        shape = engine.get_tensor_shape(binding) if profile_idx is None else engine.get_tensor_profile_shape(binding, profile_idx)[-1]
+        shape = engine.get_tensor_shape(binding) if profile_idx is None else engine.get_tensor_profile_shape(binding, profile_idx)[idx_in_profile]
         shape_valid = np.all([s >= 0 for s in shape])
         if not shape_valid and profile_idx is None:
             raise ValueError(f"Binding {binding} has dynamic shape, " +\
