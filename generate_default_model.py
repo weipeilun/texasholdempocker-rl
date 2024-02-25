f330a0650f11447add4b0f621ade89d92abe11cf
weipeilun <weipeilun0217@gmail.com>
2024-02-25 09:51:31 +0800

    fix model saving bug by TensorRT

--- generate_default_model.py
+++ generate_default_model.py
@@ -14,30 +14,12 @@ if __name__ == '__main__':
     logger.setLevel(log_level)
 
     model_name = 'models/default_train'
+    model_path = 'models/default_train.pth'
 
     args, params = parse_params()
     model_param_dict = params['model_param_dict']
     model = AlphaGoZero(**model_param_dict)
-    # to regenerate new default model
-    save_model(model, f'{model_name}.pth')
-    # to regenerate new default onnx model
-    onnx_checkpoint = f"{model_name}.onnx"
-    input_names = ['input']
-    dynamic_axes = {'input': {0: 'batch_size'}}
-    torch.onnx.export(model, torch.zeros((params['predict_batch_size_max'], *params['predict_feature_size_list']), dtype=torch.int32).to(model.device), onnx_checkpoint, opset_version=14, input_names=input_names, dynamic_axes=dynamic_axes)
 
-    # onnxsim.simplify在未知原因的特定情况下会不抛异常导致进程直接崩溃：Process finished with exit code 139 (interrupted by signal 11: SIGSEGV)。干掉这个步骤
-    # onnx_checkpoint = f"{model_name}.onnx"
-    # model_simple, is_simplify_success = onnxsim.simplify(onnx.load(onnx_checkpoint_tmp))
-    # assert is_simplify_success
-    # onnx.save(model_simple, onnx_checkpoint)
-    # os.remove(onnx_checkpoint_tmp)
-
-    TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-    trt_runtime = trt.Runtime(TRT_LOGGER)
-
-    trt_model_engine = build_engine(onnx_checkpoint, params['predict_batch_size_min'], params['predict_batch_size'], params['predict_batch_size_max'], params['predict_feature_size_list'])
-    with open(f"{model_name}.trt", "wb") as f:
-        f.write(trt_model_engine)
+    save_model_by_state_dict(model.state_dict(), model.optimizer.state_dict(), model_path, model, ModelType.TENSORRT, params)
 
     logging.info('success')
